d2ec0f432f94641b090b6f081995d175d6d3de9a
Gabriel Ferraro <gabriel.ferraro@linkapi.com.br>
2022-10-08 16:17:28 -0300

    init: First commit with some basic stuff

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import app from './app'
+
+app.listen(5555, () => {
+  console.log(`App is running on port ${5555}`)
+})
---
+++ src/services/heroes.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+
+export const list = async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({ message: 'List of heroes' })
+}
+export const get = async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({ message: 'A hero' })
+}
+
+export const insert = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).send({ message: 'hero inserted' })
+}
+export const update = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).send({ message: 'hero updated' })
+}
+
+export const del = async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({ message: 'hero deleted' })
+}
